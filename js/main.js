7ae1db05adc6b181203d1299ffcf6e034efe20dc
Kipkorir <ipkipkorir@gmail.com>
2020-02-12 18:14:55 +0300

    completed responsive navigation

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/***********************************************
+Responsive navigation
+************************************************/
+function openNav() {
+	var show_nav_bar = document.getElementById("nav-bar");
+	show_nav_bar.style.display = "flex";
+			
+	var hide_icon_menu = document.getElementById("icon-menu");
+	hide_icon_menu.style.display = "none";
+}
+
+function closeNav() {
+	var hide_nav_bar = document.getElementById("nav-bar");
+	hide_nav_bar.style.display = "none";
+	var show_icon_menu = document.getElementById("icon-menu");
+	show_icon_menu.style.display = "flex";
+}
+document.querySelector(".icon-menu").addEventListener("click", openNav);
+document.querySelector(".close-menu").addEventListener("click", closeNav);
+
